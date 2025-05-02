abe13e874789fd26c7b22f6b54b53297787c843c
Dhananjay Vishwakarma <mrstrange372@gmail.com>
2025-05-02 11:55:53 +0530

    Add files via upload

---
+++ sys.py
@@ -0,0 +1,22 @@
+import sys
+import cowsay
+
+if len(sys.argv)==2:
+    cowsay.dragon("hello ,"+sys.argv[1])
+    cowsay.pig("hello ,"+sys.argv[1])
+    cowsay.octopus("hello ,"+sys.argv[1])
+    cowsay.ghostbusters("hello ,"+sys.argv[1])
+    cowsay.turtle("hello ,"+sys.argv[1])
+    cowsay.stegosaurus("hello ,"+sys.argv[1])
+    cowsay.fox("hello ,"+sys.argv[1])
+    cowsay.tux("hello ,"+sys.argv[1])
+    cowsay.turkey("hello ,"+sys.argv[1])
+    cowsay.daemon("hello ,"+sys.argv[1])
+    cowsay.stimpy("hello ,"+sys.argv[1])
+    cowsay.milk("hello ,"+sys.argv[1])
+    cowsay.beavis("hello ,"+sys.argv[1])
+    cowsay.kitty("hello ,"+sys.argv[1])
+
+#['beavis', 'cheese', 'cow', 'daemon', 'dragon', 'fox', 'ghostbusters', 'kitty',
+#'meow', 'miki', 'milk', 'octopus', 'pig', 'stegosaurus', 'stimpy', 'trex', 
+#'turkey', 'turtle', 'tux']
